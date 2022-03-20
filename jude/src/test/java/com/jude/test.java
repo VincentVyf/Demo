368490bf2419a24fd912cef6af3d43128b13e6de
Wei <Weiyifeng0307@gmail.com>
2022-03-20 00:36:58 -0300

    Merge pull request #10 from VincentVyf/v
    Signed-off-by: Wei <Weiyifeng0307@gmail.com>

--- jude/src/test/java/com/jude/test.java
+++ jude/src/test/java/com/jude/test.java
@@ -3,6 +3,6 @@ package com.jude;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("4444444444444444444444444444");
+        System.out.println("666666666666666");
     }
 }
