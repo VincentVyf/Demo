ff9912adb075f0ae1849aa32558e061b4d257a44
VincentVyf <101956844+VincentVyf@users.noreply.github.com>
2022-03-19 23:59:02 -0300

    Update test.java

--- jude/src/test/java/com/jude/test.java
+++ jude/src/test/java/com/jude/test.java
@@ -3,7 +3,7 @@ package com.jude;
 public class test {
 
     public static void main(String[] args) {
-		sout remoteadadweeee
+		sout remoteadadweeee 7777777777777
 			sdad
 			sdfsfsfsfs 55555555555
 	fsafsafasfawww222qq   123 33333    111111111111111111111111   22222222222222222222222222222222222222222222222222222222222222
