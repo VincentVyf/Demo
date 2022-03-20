2ad464f7f6145bea631049a6c3b0d8ea873143e6
Wei <Weiyifeng0307@gmail.com>
2022-03-19 23:56:02 -0300

    Signed-off-by: Wei <Weiyifeng0307@gmail.com>

---
+++ jude/src/test/java/com/jude/test.java
@@ -0,0 +1,17 @@
+package com.jude;
+
+public class test {
+
+    public static void main(String[] args) {
+		sout remoteadadweeee
+			sdad
+			sdfsfsfsfs 55555555555
+	fsafsafasfawww222qq   123 33333    111111111111111111111111   22222222222222222222222222222222222222222222222222222222222222
+			fsafasfafafafafa
+adadadadadadaqqqqq
+			sadadada
+afasfaskfasjkfhasjfhakfas
+			dsklgdsjkghdsjghdslg
+			flsjfkslfjsklfjsl
+	fsalfjaslfjaslkf
+    }
