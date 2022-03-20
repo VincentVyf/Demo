de8665e5ecb4bef1f145e4b0e23e7376301cfe72
VincentVyf <101956844+VincentVyf@users.noreply.github.com>
2022-03-20 00:32:09 -0300

    Update test.java

--- jude/src/test/java/com/jude/test.java
+++ jude/src/test/java/com/jude/test.java
@@ -3,6 +3,6 @@ package com.jude;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("3333333333333333");
+        System.out.println("4444444444444444444444444444");
     }
 }
